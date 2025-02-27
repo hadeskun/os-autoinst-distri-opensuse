e17b77a87450ddeb3148946b389e8456254aaad9
hadeskun <jad990@gmail.com>
2025-02-27 23:40:33 +0100

    Implement new iscsi client from console
    Implement new iscsi client from console for sles16.

---
+++ tests/ha/iscsi_client_sles_16.pm
@@ -0,0 +1,48 @@
+# SUSE's openQA tests
+#
+# Copyright 2016-2020 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Package: iscsi-client-sles-16
+# Summary: Configure iSCSI target for HA tests
+# Maintainer: QE-SAP <qe-sap@suse.de>
+
+use base 'opensusebasetest';
+use strict;
+use warnings;
+use Utils::Backends qw(is_remote_backend);
+use utils qw(zypper_call systemctl ping_size_check);
+use testapi;
+use hacluster;
+use version_utils qw(is_sle package_version_cmp);
+
+sub run {
+    my $iscsi_server = get_var('USE_SUPPORT_SERVER') ? 'ns' : get_required_var('ISCSI_SERVER');
+
+    # Perform a ping size check to several hosts which need to be accessible while
+    # running this module
+    ping_size_check(testapi::host_ip());
+    ping_size_check($iscsi_server);
+
+    # open-iscsi & iscsiuio
+    if (script_run('rpm -qa open-iscsi')) {
+        zypper_call 'in open-iscsi';
+    }
+
+    my $iscsi_client = script_output("rpm -qa open-iscsi");
+    record_info('iscsi_client version', $iscsi_client);
+
+    assert_script_run("systemctl start iscsid");
+    my $iscsi_daemon_status = script_output("systemctl status iscsid");
+    record_info('iscsi_client version', $iscsi_daemon_status);
+    if($iscsi_daemon_status =~ /Active: active \(running\) since/) {
+        my $iqn_node_name = script_output("iscsiadm -m discovery -t st -p 10.0.2.1|grep 'openqa'");
+        record_info('iscsi_iqn_node', $iqn_node_name);
+        my ($node_name) = $iqn_node_name =~ /(\S+)$/;
+        record_info('node_name', $node_name);
+        assert_script_run("iscsiadm -m node --targetname '$node_name' --login");
+        assert_script_run("iscsiadm -m session");
+    }
+}
+
+1;
